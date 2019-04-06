e49e529b4e62986674df2abd50ceb8d373a313c5
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-04-06 13:13:04 +0100

    Added functionality with sample pics

---
+++ lib/cards.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'color_generator.dart';
+
+class Cards extends StatefulWidget {
+  @override
+  _CardsState createState() => _CardsState();
+}
+
+class _CardsState extends State<Cards> {
+
+  Widget _buildBackgroundColor(){
+    return new ColorGenerator(
+      colors: [
+        'assets/color1.png',
+        'assets/color2.png',
+        'assets/color3.png',
+        'assets/color4.png',
+      ],
+      visibleColorIndex: 0,
+    );
+  }
+
+  Widget _buildQuote(){
+    return new Positioned(
+      left: 0.0,
+      right: 0.0,
+      bottom: 0.0,
+      child: new Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.transparent,
+              Colors.black.withOpacity(0.8),
+            ],
+          ),
+        ),
+        padding: const EdgeInsets.all(24.0),
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Text(
+                    "Quote here",
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.circular(10.0),
+        boxShadow: [
+          new BoxShadow(
+            color: const Color(0x11000000),
+            blurRadius: 5.0,
+            spreadRadius: 2.0,
+          ),
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: new Material(
+          child: new Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              _buildBackgroundColor(),
+              _buildQuote(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/color_generator.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class ColorGenerator extends StatefulWidget {
+  final List<String> colors;
+  final int visibleColorIndex;
+
+  ColorGenerator({
+    this.colors,
+    this.visibleColorIndex,
+  });
+
+  @override
+  _ColorGeneratorState createState() => _ColorGeneratorState();
+}
+
+class _ColorGeneratorState extends State<ColorGenerator> {
+  int visiblePhotoIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    visiblePhotoIndex = widget.visibleColorIndex;
+  }
+
+  @override
+  void didUpdateWidget(ColorGenerator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.visibleColorIndex != oldWidget.visibleColorIndex) {
+      setState(() {
+        visiblePhotoIndex = widget.visibleColorIndex;
+      });
+    }
+  }
+
+  void _prevImage() {
+    setState(() {
+      visiblePhotoIndex = visiblePhotoIndex > 0 ? visiblePhotoIndex - 1 : 0;
+    });
+  }
+
+  void _nextImage() {
+    setState(() {
+      visiblePhotoIndex = visiblePhotoIndex < widget.colors.length - 1
+          ? visiblePhotoIndex + 1
+          : visiblePhotoIndex;
+    });
+  }
+
+  Widget _buildPhotoControls() {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        new GestureDetector(
+          onTap: _prevImage,
+          child: new FractionallySizedBox(
+            widthFactor: 0.5,
+            heightFactor: 1.0,
+            alignment: Alignment.topLeft,
+            child: new Container(
+              color: Colors.transparent,
+            ),
+          ),
+        ),
+        new GestureDetector(
+          onTap: _nextImage,
+          child: new FractionallySizedBox(
+            widthFactor: 0.5,
+            heightFactor: 1.0,
+            alignment: Alignment.topRight,
+            child: new Container(
+              color: Colors.transparent,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        // Photo
+        new Image.asset(
+          widget.colors[visiblePhotoIndex],
+          fit: BoxFit.cover,
+        ),
+
+        // Photo Indicator
+        new Positioned(
+          top: 0.0,
+          left: 0.0,
+          right: 0.0,
+        ),
+
+        // Photo Controls
+        _buildPhotoControls(),
+      ],
+    );
+  }
+}
