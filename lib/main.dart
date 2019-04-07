0621b703eccb840a35d27a09fb7321cccd9514eb
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-04-07 18:15:49 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _helpDialog(){
+  /*void _helpDialog(){
     showDialog(
         context: context,
         builder: (_) => new AlertDialog(
@@ -43,11 +43,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 new Text("Tap on the right side of the picture to see the next one →"
                   "\n\nTap on the left side of the picture to see the previous one ←"
                   "\n\nFor suggestions on more student life hacks, please email: "),
+                new InkWell(
+                  child: new Text("\nstudentlifehacks@gmail.com", style: TextStyle(color: Colors.blue)),
+                  onTap: () async {
+                    await launch("mailto:studentlifehacks@gmail.com");
+                  },
+                ),
               ],
           ),
         )
     );
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         iconSize: MediaQuery.of(context).size.height/23.5,
                         color: Colors.white,
                         onPressed: () => {
-                          _helpDialog()
+                          //_helpDialog()
                         }
                   ),
                   new Text("Siddharth Notani", style: TextStyle(color: Colors.white)),
