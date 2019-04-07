9433a976f233f9da80ef26a71c7111236763c50c
Siddharth Notani <36894543+snotani@users.noreply.github.com>
2019-04-07 18:30:48 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -42,12 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 new Text("Tap on the right side of the picture to see the next one →"
                   "\n\nTap on the left side of the picture to see the previous one ←"),
-                new InkWell(
-                  child: new Text("\nstudentlifehacks@gmail.com", style: TextStyle(color: Colors.blue)),
-                  onTap: () async {
-                    await launch("mailto:studentlifehacks@gmail.com");
-                  },
-                ),
               ],
           ),
         )
